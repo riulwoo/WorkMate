bfff088f8f607627155ac8d22db7e6069319382c
Riulwoo <geun10121@naver.com>
2022-02-14 16:11:18 +0900

    add match

--- WorkMate/server.js
+++ WorkMate/server.js
@@ -29,6 +29,8 @@ class PlayerBall{
         this.socket = socket;
         this.x = startX;
         this.y = startY;
+        this.score = 0;
+        this.nick = "player";
         this.color = getPlayerColor(); // 백그라운드 이미지로 대체
     }
 
@@ -37,27 +39,44 @@ class PlayerBall{
     }
 }
 
-var balls = [];
-var ballMap = {};
+var userpool = [];  // 총 인원
+var matchinguser = [];                    // 게임참여인원은 빠짐
+var userinfo = {};
+
+
+class userroom{
+    constructor(){
+        player1
+        player2
+        player3
+        player4
+        player5
+        player6
+    }
+}
 
 function joinGame(socket){
-    let ball = new PlayerBall(socket);
+    let player = new PlayerBall(socket);
 
-    balls.push(ball);
-    ballMap[socket.id] = ball;
+    userpool.push(player);
+    matchinguser.push(player); 
+    userinfo[socket.id] = player;
 
-    return ball;
+    return player;
 }
 
 function endGame(socket){
-    for( var i = 0 ; i < balls.length; i++){
-        if(balls[i].id == socket.id){
-            balls.splice(i,1);
+    for( var i = 0 ; i < userpool.length; i++){
+        if(userpool[i].id == socket.id){
+            userpool.splice(i,1);
             break
         }
     }
-    delete ballMap[socket.id];
+    delete userinfo[socket.id];
 }
+
+
+
 
 io.on('connection', function(socket) {
     console.log(`${socket.id}님이 입장하셨습니다.`);
@@ -68,24 +87,24 @@ io.on('connection', function(socket) {
         socket.broadcast.emit('leave_user', socket.id);
     });
 
-    let newBall = joinGame(socket);
+    let newplayer = joinGame(socket);
     socket.emit('user_id', socket.id);
 
-    for (var i = 0 ; i < balls.length; i++){
-        let ball = balls[i];
+    for (var i = 0 ; i < userpool.length; i++){
+        let player = userpool[i];
         socket.emit('join_user', {
-            id: ball.id,
-            x: ball.x,
-            y: ball.y,
-            color: ball.color,
+            id: player.id,
+            x: player.x,
+            y: player.y,
+            color: player.color,
         });
     }
 
     socket.broadcast.emit('join_user',{
         id: socket.id,
-        x: newBall.x,
-        y: newBall.y,
-        color: newBall.color,
+        x: newplayer.x,
+        y: newplayer.y,
+        color: newplayer.color,
     });
 
     socket.on('send_location', function(data) {
@@ -95,4 +114,37 @@ io.on('connection', function(socket) {
                 y: data.y,
             })
     })
+
+    socket.on('matching', function(){
+        if(matchinguser.length == 1)
+        let Timer = 30;
+        let matchuser = matchinguser.length;
+        setInterval(MatchTimer(Timer, matchuser),1000)
+        socket.emit('gamestart', {
+            Timer: Timer
+        });
+    })
 })
+
+
+// 타이머 함수
+function MatchTimer(timer, matchuser){
+    // 타이머 변수, 타이머 줄어드는 기능, 유저풀 체크 로직, 클라이언트에 메시지전달, 화면전환
+    // 1명일때부터 타이머가 돌아가는데 2명일때는 타이머변수가 변하지않음 방장이 나가도 타이머는 돌아간다
+    
+    // 타이머 줄어드는 기능
+    
+    // 매칭중인 유저들 수 체크
+
+    // 클라이언트에 메시지전달과 화면전환
+
+    if(timer == 0 && matchuser >= 2)
+    // 랜덤으로 게임 화면 전환
+    matchinguser의 내용 지우기
+    sendFile(__dirname + '/views/index.html')
+    if(matchuser == 6)
+    // 랜덤으로 게임 화면 
+    matchinguser의 내용 지우기
+    sendFile(__dirname + '/views/index.html')
+    clearInterval
+}
---
+++ WorkMate/views/useful.js
@@ -0,0 +1,156 @@
+function renderClearMessage(){
+    ctx.beginPath();
+    ctx.fillStyle = '#000000';
+    ctx.font = '50px Arial';
+    ctx.fillText(`Stage ${stage-1} Clear!!`,1024/2-150, 730-50);
+    ctx.fillText(`The next stage will start shortly.`,1024/2-350, 730);
+    ctx.closePath();
+}
+
+function start(){
+    if(!isStart){
+        socket.emit('start', { id: myId, stage : stage, waiting : false, isStart : true});
+    }
+}
+
+socket.on('start_game', function(){
+    isStart = true;
+    let bgm = document.getElementById("bgm");
+    bgm.volume = 0.3;
+    bgm.play();
+})
+
+let stageClear = false;
+        socket.on('stage_clear',function(data){//스테이지 하나가 끝난 상태
+            stageClear = true;
+            enemys.length = 0;
+            straightEnemys.length = 0;
+            items.length = 0;
+            for (var i = 0 ; i < balls.length ; i++){
+                if( balls[i].getState() == 0){
+                    balls[i].setX(1024/2);
+                    balls[i].setY(768/2);
+                    balls[i].setState(1);
+                }
+            }
+
+            isStart = false;
+            socket.emit('start', { id: myId, stage : stage, waiting : true});//웨이팅 스테이지로 이동
+            stage = data.stage;//스테이지 1 업 시켜주기
+        })
+        socket.on('end_waiting', function(){//웨이팅이 끝난상태
+            timer = 15;
+            stageClear = false;
+            isStart = false;
+            socket.emit('start', { id: myId, stage : stage, waiting : false});            
+        })
+
+        socket.on('game_over', function(data){ //전 플레이어가 죽으면 나오는 이벤트
+            if (data.isFail){
+                stage = 1;
+                Swal.fire({
+                    title: "게임 알림",
+                    text: "교수님이 이겼습니다.",
+                    confirmButtonText: "아니..",
+                    confirmButtonColor: '#FC5296'
+                }).then((result)=> {
+
+                    location.href= "/bad";
+                })
+            }
+        })
+
+        socket.on('game_win', function(data){
+            stage = 1;
+            Swal.fire({
+                title: "게임 알림",
+                text: "당신이 이겼습니다! 축하드립니다.",
+                confirmButtonText: "확인",
+                confirmButtonColor: '#FC5296'
+            }).then((result) => {
+                location.href= "/good";
+            })
+        })
+
+        function enterGame(){
+            var enter = document.getElementById('enter').value
+            if(enter.length === 0){
+                Swal.fire({
+                    title: "게임 알림",
+                    text: "닉네임을 입력해주세요",
+                    confirmButtonText: "네",
+                    confirmButtonColor: '#FC5296'
+                });
+            }else if( enter.length < 2 || enter.length > 9 ){
+                Swal.fire({
+                    title: "게임 알림",
+                    text: "닉네임은 최소 2자에서 최대 8자 입니다.",
+                    confirmButtonText: "네",
+                    confirmButtonColor: '#FC5296'
+                });
+            }else{
+                localStorage.setItem("nickName", enter);
+                location.href= "/game"
+            }
+        
+        
+        }
+
+        function renderPlayers(){
+            let curPlayer = ballMap[myId];
+            for (let i = 0; i < balls.length; i++) {
+                    let ball = balls[i];
+                    if (ball.getState() == 0){
+                        continue
+                    }
+                    ctx.beginPath();
+                    ctx.fillStyle = ball.getColor();
+                    ctx.arc(ball.getX(), ball.getY(), ball.getRadius(), 0, Math.PI * 2, false);
+                    ctx.fill();
+                    ctx.closePath();
+
+                    if( ball == curPlayer){
+
+                        ctx.beginPath();
+                        ctx.font = '15px Arial';
+                        ctx.fillText(`${nickName}`,ball.getX()- ball.getRadius()-7, ball.getY() - ball.getRadius());
+                        ctx.closePath();
+                    }else{
+                        ctx.beginPath();
+                        ctx.font = '15px Arial';
+                        ctx.fillText(`player${i}`,ball.getX()- ball.getRadius()-7, ball.getY() - ball.getRadius());
+                        ctx.closePath();
+                    }
+                }
+                
+                if (rightPressed){
+                    if (curPlayer.getX() <= 1024 - curPlayer.getRadius()){
+                        curPlayer.setX(curPlayer.getX() + curPlayer.getPlayerSpeed());
+                    }
+                }
+                if (leftPressed ){
+                    if(curPlayer.getX() >= 0 + curPlayer.getRadius()){
+                        curPlayer.setX(curPlayer.getX() - curPlayer.getPlayerSpeed());
+                    }
+                }
+                if(upPressed ){
+                    if(curPlayer.getY() >= 0 + curPlayer.getRadius()){
+                        curPlayer.setY(curPlayer.getY() - curPlayer.getPlayerSpeed());
+                    }
+                }
+                if(downPressed ){
+                    if(curPlayer.getY() <= 768 - curPlayer.getRadius()){
+                        curPlayer.setY(curPlayer.getY() + curPlayer.getPlayerSpeed());
+                    }
+                }
+        }
+
+        let timer = 15.00;
+        function renderTimer(){
+            ctx.beginPath();
+            ctx.fillStyle = '#000000';
+            ctx.font = '20px Arial';
+            ctx.fillText(`Timer ${timer.toFixed(2)}`,30,50);
+            ctx.closePath();
+
+        }
