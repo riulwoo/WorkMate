1790744805bd9354aeecd033a91069968f67aa6f
mins <80028813+minseong00@users.noreply.github.com>
2022-05-25 16:15:22 +0000

    ...

--- WorkMate/testserver.js
+++ WorkMate/testserver.js
@@ -62,7 +62,7 @@ class userroom {  // 클라이언트 코드에도 작성해야함 : 같이 플�
   this.gameName;
 
   // 플레이어 1~6명의 정보
-    this.players = [];
+    this.players = [6];
     for (let i = 0; i < 6; i++) {
       this.players.push({ id: null, nick: null, score: null });
     }
