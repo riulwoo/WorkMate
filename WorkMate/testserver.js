c19c6e8e2e9221e7263fb0c5c8b934fb5fad5477
mins <80028813+minseong00@users.noreply.github.com>
2022-05-27 12:44:27 +0000

    ...

--- WorkMate/testserver.js
+++ WorkMate/testserver.js
@@ -283,7 +283,7 @@ io.on('connection', function(socket) {
         {
           if(checkid[i] === id) 
           {
-            io.to(temp.roomCode).emit('gamestart', "/views/gamebase.html");
+            io.to(temp.roomCode).emit('gamestart', "./gamebase.html");
           }
         }
       });
