e30183b8b82f0ebf895dfba29fb248b09c2da2ad
leegeunwoo <67493361+riulwoo@users.noreply.github.com>
2022-06-09 06:00:08 +0000

    220609 create folder

--- WorkMate/realserver.js
+++ WorkMate/realserver.js
@@ -13,7 +13,7 @@ server.listen(process.env.PORT || 3000, ()=> {
 app.use(express.static('views'))
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+  res.sendFile(__dirname + '/test/index.html')
 })
 
 app.get('/gamebase', (req, res) => {
