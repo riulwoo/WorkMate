386c893d0b196efb723868d144d2f901cdf2838a
mins <80028813+minseong00@users.noreply.github.com>
2022-07-22 09:20:05 +0000

    ...

--- WorkMate/realserver.js
+++ WorkMate/realserver.js
@@ -13,7 +13,7 @@ server.listen(process.env.PORT || 3000, ()=> {
 //app.use(express.static('views'))
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/gamebase.html')
+  res.sendFile(__dirname + '/views/game/space_race/index.html')
 })
 
 
