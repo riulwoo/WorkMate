43391f5f29e49222b4c455d0e5702201b683f125
mins <80028813+minseong00@users.noreply.github.com>
2022-07-22 08:40:19 +0000

    ...

--- WorkMate/views/script.js
+++ WorkMate/views/script.js
@@ -76,7 +76,7 @@ socket.on('user_id', function(data){
 
 socket.on('gamestart', function() {
 console.log('게임 스타트');
-$('#main').load('./game/space_race/index.html');
+$('#main').load('/gamebase.html');
 // var ajaxOption = {
 //       url : "./game/space_race/index.html",
 //       async : true,
