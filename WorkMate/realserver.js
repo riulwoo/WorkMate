f2ab76c0e23ad04565105364e8b77c825c356ef6
mins <80028813+minseong00@users.noreply.github.com>
2022-07-26 05:51:40 +0000

    ...

--- WorkMate/realserver.js
+++ WorkMate/realserver.js
@@ -140,8 +140,10 @@ io.on('connection', function(socket) {
             {
               socket.leave(room[i].roomCode);
               
-              if(room[i].deleteUser(id, j)) room[i].roomCode = null;
-              
+              if(room[i].deleteUser(id, j)) {
+                room[i].roomCode = null;
+                room[i].check = null;
+              }
               console.log('[matchcancel] leave 후 조인 방 정보 : ' + room[i].roomCode);
               console.log('[matchcancel] 유저 정보삭제 후 정보 : ' + room[i].userid);
             }
