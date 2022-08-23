09c7a79ab53f08426e95147011aac9f64448fc96
leegeunwoo <67493361+riulwoo@users.noreply.github.com>
2022-08-23 01:37:59 +0000

    220823 메인 페이지 수정

--- WorkMate/views/script.js
+++ WorkMate/views/script.js
@@ -4,8 +4,11 @@ var Croombtn = document.getElementById("createroom"); //방만들기 버튼
 var Jroombtn = document.getElementById("joinroom"); //방 입장 버튼
 var start = document.getElementById("start"); //게임 시작 버튼
 var nickname = document.getElementById("nick"); // 닉네임
+nickname = nickname.value == null || nickname.value == undefined || nickname.value == '' ?
+  "Player " + Math.floor(Math.random()*100+1) : nickname.value 
+console.log(nickname)
 var rmcodetxt = document.getElementById("roomcode"); // 입력받은 룸 코드
-let readercode = document.getElementById("readerCode");
+let leadercode = document.getElementById("readerCode");
 var roomid = '';
 var a = 1;
 
@@ -15,19 +18,18 @@ Matchbtn.addEventListener("click", match);
 Croombtn.addEventListener("click", function () {
   roomid = (new Date().getTime() + Math.random()).toString(36).substring(2,7);
   
-  readercode.innerText = roomid;
-  console.log("create room 눌림 " + myId + roomid + ' ' + nickname.value);
+  leadercode.innerText = roomid;
+  console.log("create room 눌림 " + myId + roomid + ' ' + nickname);
   socket.emit('createroom', {
     id : myId, 
     roomid : roomid, 
-    nick : nickname.value,
+    nick : nickname,
     score : 0
   }); 
   toggleRoom();
   let slot = document.querySelectorAll(".slot")
   let name = document.createElement('div')
-  let nick = nickname.value != undefined ? nickname.value : `player 0`; 
-  let nickNode = document.createTextNode(nick)
+  let Node = document.createTextNode(nickname)
   let img = document.createElement('img')
   name.classList.add('in_slot_name');
   name.appendChild(nickNode);
@@ -40,11 +42,10 @@ Jroombtn.addEventListener('click', function () {
   console.log('join room 눌림');
   if(rmcodetxt.value == null || rmcodetxt.value == '')
     alert('방 코드를 입력해주세요\n' + '입력받은 방코드 : ' + rmcodetxt.value)
-  if(nickname.value == null || nickname.value == undefined || nickname.value == '') nickname = "Player" + Math.floor(Math.random()*100+1)
   socket.emit('joinroom', {
     id : myId, 
     roomid : rmcodetxt.value,
-    nick : nickname.value,
+    nick : nickname,
     score : 0
   }); 
   toggleRoom();
@@ -52,7 +53,7 @@ Jroombtn.addEventListener('click', function () {
 })
 
 socket.on('joinsuccess', (data)=>{
-  readercode.innerText = data.roomcode;
+  leadercode.innerText = data.roomcode;
 })
 socket.on('joinfail', ()=>{
   alert('올바른 코드를 입력해주세요!');
@@ -80,13 +81,12 @@ socket.on('gamestart', function(data) {
 })
 
 function match(e) {
-  let nickname = "nickname " + a;
   roomid  =  (new Date().getTime() + Math.random()).toString(36).substring(2,7);
   
   socket.emit("matchStart", {
     id : myId,
     roomid : roomid,
-    nick : nickname.value, 
+    nick : nickname, 
     score : 0
   });
   console.log("매치 시작 보냈다?");
