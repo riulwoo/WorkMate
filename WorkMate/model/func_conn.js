739f4b4c019c556dccf1e073b3e7db85240b50b3
mins <80028813+minseong00@users.noreply.github.com>
2022-09-08 03:17:16 +0000

    ..

--- WorkMate/model/func_conn.js
+++ WorkMate/model/func_conn.js
@@ -1,33 +1,7 @@
 const userroom = require('./class_room');
-const mainHandlers = require('../socketHandler/mainHandler');
-const oxHandlers = require('../socketHandler/oxHandler');
-const flipHandlers = require('../socketHandler/flipHandler');
-const raceHandlers = require('../socketHandler/raceHandler');
 
-function init() {
+module.exports = function init() {
   let room = new Array();
   room[0] = new userroom();
   return room;
-}
-
-function onConnection(socket, room) {
-  console.log(`${socket.id}님이 입장하셨습니다.`);
-  mainHandlers(io, socket, room);
-  oxHandlers(io, socket, room);
-  flipHandlers(io, socket, room);
-  raceHandlers(io, socket, room);
-  
-  //   // 나중에 게임 연결 성공하면 to(room)에게 보내주는 형태로 수정
-  // socket.on('send_location', function(data) {
-  //         socket.broadcast.emit('update_state', {
-  //             id: data.id,
-  //             x: data.x,
-  //             y: data.y,
-  //         })
-  // })
-}
-
-module.exports = {
-  init,
-  onConnection
 }
--- WorkMate/testserver.js
+++ WorkMate/testserver.js
@@ -8,8 +8,11 @@ const io = require('socket.io')(server);
 const Router = require('./router/pageRouter');
 
 /** Import SocketHandler */
-
-const { init, onConnection } = require('./model/func_conn');
+const init = require('./model/func_conn');
+const mainHandlers = require('../socketHandler/mainHandler');
+const oxHandlers = require('../socketHandler/oxHandler');
+const flipHandlers = require('../socketHandler/flipHandler');
+const raceHandlers = require('../socketHandler/raceHandler');
 
 /** Set Middleware */
 app.use(express.static('views'));
@@ -17,6 +20,22 @@ app.use(express.static('game'));
 
 /** Set Routers */
 app.use('*', Router);
+
+function onConnection(socket, room) {
+  console.log(`${socket.id}님이 입장하셨습니다.`);
+  mainHandlers(io, socket, room);
+  oxHandlers(io, socket, room);
+  flipHandlers(io, socket, room);
+  raceHandlers(io, socket, room);
+  //   // 나중에 게임 연결 성공하면 to(room)에게 보내주는 형태로 수정
+  // socket.on('send_location', function(data) {
+  //         socket.broadcast.emit('update_state', {
+  //             id: data.id,
+  //             x: data.x,
+  //             y: data.y,
+  //         })
+  // })
+}
 
 let room = init();
 io.on('connection', onConnection(room));
