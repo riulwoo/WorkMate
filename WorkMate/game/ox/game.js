3d8db467ca527a61d8354c7eefe0b6f611953b7c
leegeunwoo <67493361+riulwoo@users.noreply.github.com>
2022-09-08 06:30:37 +0000

    ...

--- WorkMate/game/ox/game.js
+++ WorkMate/game/ox/game.js
@@ -1,13 +1,44 @@
 let canvas = document.getElementById("ox_canvas");
 let ctx = canvas.getContext('2d');
 let myfont = new FontFace('DungGeunMo', 'url(ox/assets/fonts/DungGeunMo.otf)');
-import player from "./player.js";
+//const player = require("./player.js");
 
 
 myfont.load().then(function(font){
     document.fonts.add(font);
     console.log('font loaded.');
 });
+
+function player(nick)
+{
+    // 플레이어 닉네임 설정
+    if (nick == null)
+    {
+        this.nick = "player" + Math.floor(Math.random() * 100);
+    }
+    else
+    {
+        this.nick = nick;
+    }
+
+    this.asset = [ // 플레이어 이동 시 출력 될 이미지.
+        // 순서대로 정면(방향키 아래), 좌측, 후면(방향키 위), 우측 
+        'https://cdn.discordapp.com/attachments/980090904394219562/1004271208226881606/1.png',
+        'https://cdn.discordapp.com/attachments/980090904394219562/1004271240271376385/4.png',
+        'https://cdn.discordapp.com/attachments/980090904394219562/1004271284735193139/4.png',
+        'https://cdn.discordapp.com/attachments/980090904394219562/1004271430722146345/3.png'
+    ];
+
+    this.color = "#FF00FF";
+    this.x = 1200 / 2;
+    this.y = 800 / 2;
+    this.player = new Image();
+    this.player.src = this.asset[0];
+    this.score = 0;
+
+    // 판정 관련
+    this.is_O;
+}
 
 // 게임의 프레임은 60fps.
 const FPS = 60;
