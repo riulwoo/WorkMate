06d2014261b12657e209f581f2e3dac6e710404f
mins <80028813+minseong00@users.noreply.github.com>
2022-09-08 01:45:43 +0000

    ...

--- WorkMate/router/pageRouter.js
+++ WorkMate/router/pageRouter.js
@@ -2,19 +2,19 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/ox', (req, res) => {
-  res.sendFile(__dirname + '/game/ox/index.html');
+  res.sendFile('../game/ox/index.html');
 });
 
 router.get('/space', (req, res) => {
-  res.sendFile(__dirname + '/game/space/index.html');
+  res.sendFile('../game/space/index.html');
 });
 
 router.get('/filpOver', (req, res) => {
-  res.sendFile(__dirname + '/gamebase.html');
+  res.sendFile('../views/gamebase.html');
 });
 
 router.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html');
+  res.sendFile('../views/index.html');
 });
 
 module.exports = router;
