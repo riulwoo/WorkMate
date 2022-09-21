f7ca4cf83c3921968291544dd25cf743d3f5111e
leegeunwoo <67493361+riulwoo@users.noreply.github.com>
2022-09-21 03:30:35 +0000

    ...

--- WorkMate/router/pageRouter.js
+++ WorkMate/router/pageRouter.js
@@ -1,21 +1,28 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/ox', (req, res) => {
-  res.sendFile('game/ox/index.html');
-});
-
-router.get('/space', (req, res) => {
-  res.sendFile('game/space/index.html');
+router.get('/result', (req, res) => {
+  res.sendFile('views/resultpage/index.html');
+  console.log('result 전송완료');
 });
 
 router.get('/flipover', (req, res) => {
   res.sendFile('game/flipover/index.html');
+  console.log('flipover 전송완료');
 }); 
 
-router.get('/result', (req, res) => {
-  res.sendFile('views/resultpage/index.html');
-  console.log('result 전송완료')
+router.get('/ox', (req, res) => {
+  res.sendFile('game/ox/index.html');
+  console.log('ox 전송완료')
 });
+
+router.get('/space', (req, res) => {
+  res.sendFile('game/space/index.html');
+  console.log('space 전송완료')
+});
+
+
+
+
 
 module.exports = router;
