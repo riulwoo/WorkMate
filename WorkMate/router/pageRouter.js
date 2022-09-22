c75c5ba8cbd4bf1b909bdb34402325f7de632596
leegeunwoo <67493361+riulwoo@users.noreply.github.com>
2022-09-22 01:37:52 +0000

    ...

--- WorkMate/router/pageRouter.js
+++ WorkMate/router/pageRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/resulting', (req, res) => {
-  res.sendFile('views/resultpage/index.html');
+  res.sendFile('resultpage/index.html');
 });
 
 router.get('/flipover', (req, res) => {
