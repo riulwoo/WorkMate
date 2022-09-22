b73f1d95997479eaefd55e8293e15066d80167e2
mins <80028813+minseong00@users.noreply.github.com>
2022-09-22 01:52:57 +0000

    ...

--- WorkMate/router/pageRouter.js
+++ WorkMate/router/pageRouter.js
@@ -21,18 +21,18 @@
 module.exports = function(app)
 {
 app.get('/result', (req, res) => {
-  res.sendFile('../views/resultpage/index.html');
+  res.sendFile(__dirname + 'views/resultpage/index.html');
 });
 
 app.get('/flipover', (req, res) => {
-  res.sendFile('../game/flipover/index.html');
+  res.sendFile(__dirname + 'game/flipover/index.html');
 }); 
 
 app.get('/ox', (req, res) => {
-  res.sendFile('../game/ox/index.html');
+  res.sendFile(__dirname + 'game/ox/index.html');
 });
 
 app.get('/space', (req, res) => {
-  res.sendFile('../game/space/index.html');
+  res.sendFile(__dirname + 'game/space/index.html');
 });
 }
