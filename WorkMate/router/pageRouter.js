5dba0f4e0dbf84c7b250a7a310eb3875140d20b3
leegeunwoo <67493361+riulwoo@users.noreply.github.com>
2022-09-22 01:49:54 +0000

    ...

--- WorkMate/router/pageRouter.js
+++ WorkMate/router/pageRouter.js
@@ -1,7 +1,7 @@
 // const express = require('express');
 // const router = express.Router();
 
-// router.get('/resulting', (req, res) => {
+// router.get('/result', (req, res) => {
 //   res.sendFile('resultpage/index.html');
 // });
 
@@ -21,18 +21,18 @@
 module.exports = function(app)
 {
 app.get('/result', (req, res) => {
-  res.sendFile('views/resultpage/index.html');
+  res.sendFile('../views/resultpage/index.html');
 });
 
 app.get('/flipover', (req, res) => {
-  res.sendFile('game/flipover/index.html');
+  res.sendFile('../game/flipover/index.html');
 }); 
 
 app.get('/ox', (req, res) => {
-  res.sendFile('game/ox/index.html');
+  res.sendFile('../game/ox/index.html');
 });
 
 app.get('/space', (req, res) => {
-  res.sendFile('game/space/index.html');
+  res.sendFile('../game/space/index.html');
 });
 }
