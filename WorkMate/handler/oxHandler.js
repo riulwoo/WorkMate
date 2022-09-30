57e62e76d9d60e2a231ad6c1eff9878ae129da9b
leegeunwoo <67493361+riulwoo@users.noreply.github.com>
2022-09-30 07:05:09 +0000

    220930 ox test

--- WorkMate/handler/oxHandler.js
+++ WorkMate/handler/oxHandler.js
@@ -2,22 +2,28 @@ const [question, answer] = require('./data_quiz');
 const userroom = require("./class_room");
 const mainHandlers = require('./handler/mainHandler');
 
-// 타이머 문제
-// 근우쿤은 OX게임에 사용할 타이머코드를 작성하려한다.
-// 7문제동안 준비시간, 문제시간, 체킹시간을 나누어 게임을 진행하려 한다.
-// 이 코드를 작성하시오
+const BREAK_DUR_TIME = 3999; // 퀴즈와 퀴즈 사이의 대기 시간    (ms)
+const QUIZ_DUR_TIME = 5999; // 문제 출력 후 퀴즈 진행 시간      (ms)
+const CHECK_DUR_TIME = 999; // 퀴즈를 풀고 난 뒤 정답 체크 시간  (ms)
 
 module.exports = (io, socket, room) => {
-  
-  const promise = new Promise((resolve, reject) => {
-    setTimeout(() => {
-      
-    }) 
-    // breaking메시지 보내는 코드 5.9초후에
-    // during 메시지를 보내는 코드 3.9초후에
-    // checking 메시지를 보내는 코드 
-  }) 
-  
+  const oxcycle1 = (ms, Index)=>{
+    return new Promise((resolve)=>
+      setTimeout(()=>{
+        if(ms == 5900)      io.to(room[Index].roomCode).emit('ox_breaking', {break_time : 4, question : "강민성 바보"});
+        else if(ms == 3900) io.to(room[Index].roomCode).emit('ox_during', {during_time : 6, answer : true});
+        else if(ms == 900)  io.to(room[Index].roomCode).emit('ox_checking', {check_time : 1});
+        resolve(ms);
+      },ms)
+    );
+  };
+
+  const cycle1 = async (Index)=>{
+    const isbreak = await oxcycle1(BREAK_DUR_TIME, Index);
+    const isduring = await oxcycle1(QUIZ_DUR_TIME, Index);
+    const ischeck = await oxcycle1(CHECK_DUR_TIME, Index);
+  };
+
   socket.on('쥰비완료쓰', (id)=>{
     let userRoomIndex = getRoomIndex(id);
     if (userRoomIndex !== -1) {
@@ -25,7 +31,7 @@ module.exports = (io, socket, room) => {
       let player = room[userRoomIndex].players;
       if(room[userRoomIndex].cnt == player.length) {
           io.to(room[userRoomIndex].roomCode).emit('ox_breaking');
-        
+          cycle1(userRoomIndex);
       }
     }
   })
@@ -33,6 +39,3 @@ module.exports = (io, socket, room) => {
 
 
 
-const QUIZ_DUR_TIME = 5.9; // 문제 출력 후 퀴즈 진행 시간
-const BREAK_DUR_TIME = 3.9; // 퀴즈와 퀴즈 사이의 대기 시간
-const CHECK_DUR_TIME = 0.9; // 퀴즈를 풀고 난 뒤 정답 체크 시간
