66980433744af49f453bcc6af62fd91c00a7daa0
mins <80028813+minseong00@users.noreply.github.com>
2022-09-30 07:49:31 +0000

    ...

--- WorkMate/handler/oxHandler.js
+++ WorkMate/handler/oxHandler.js
@@ -6,8 +6,6 @@ const QUIZ_DUR_TIME = 5999; // 문제 출력 후 퀴즈 진행 시간      (ms)
 const CHECK_DUR_TIME = 999; // 퀴즈를 풀고 난 뒤 정답 체크 시간  (ms)
 
 module.exports = (io, socket, room) => {
-    
-    
   
   const oxcycle1 = (ms, Index)=>{
     return new Promise((resolve)=>
@@ -27,7 +25,7 @@ module.exports = (io, socket, room) => {
   };
 
   socket.on('쥰비완료쓰', (id)=>{
-    let userRoomIndex = room.findIndex(e => e.userid.includes(Id));
+    let userRoomIndex = room.findIndex(e => e.userid.includes(id));
     if (userRoomIndex !== -1) {
       room[userRoomIndex].cnt += 1;
       let player = room[userRoomIndex].players;
