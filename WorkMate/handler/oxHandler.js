f31409c4749b297a09288a43ba83eee4d8f6f45c
mins <rkdalsrl20@gmail.com>
2022-10-06 12:06:13 +0900

    ox cyclce

--- WorkMate/handler/oxHandler.js
+++ WorkMate/handler/oxHandler.js
@@ -4,69 +4,66 @@ const userroom = require("./class_room");
 const BREAK_DUR_TIME = 3999; // 퀴즈와 퀴즈 사이의 대기 시간    (ms)
 const QUIZ_DUR_TIME = 5999; // 문제 출력 후 퀴즈 진행 시간      (ms)
 const CHECK_DUR_TIME = 999; // 퀴즈를 풀고 난 뒤 정답 체크 시간  (ms)
+const quiz_num = [1, 2, 3, 4, 5, 6, 7, 8];
 module.exports = (io, socket, room) => {
 
   function random_quiz(ms, Index) {
     var quiz_index = Math.floor(Math.random() * question.length);
     while(true) {
-      if(room[Index].cur_quiz_index.length == 1) {
-        room[Index].cur_quiz_index.push(quiz_index);
-        return answer[quiz_index];
-      }
-      else if(room[Index].cur_quiz_index.includes(quiz_index))
+      if(room[Index].cur_quiz_index.includes(quiz_index))
       {
         quiz_index = Math.floor(Math.random() * question.length);
       }
       else if(!(room[Index].cur_quiz_index.includes(quiz_index))){
+        room[Index].cur_quiz_index.push(quiz_index);
         if(ms == 3999) {
-          room[Index].cur_quiz_index.push(quiz_index);
           return answer[quiz_index];
         }
         if(ms == 999) {
-          room[Index].cur_quiz_index.push(quiz_index);
           return question[quiz_index];
         }
       }
     }
-
-
   }
   
-  const oxcycle1 = (ms, Index)=>{
+  const oxcycle = (ms, Index)=>{
     return new Promise((resolve, reject)=> {
       setTimeout(()=>{
         if(ms == 5999)      {
-          io.to(room[Index].roomCode).emit('ox_checking', {check_time : 1});
+          io.to(room[Index].roomCode).emit('ox_checking', { check_time: 1 });
           resolve(ms);
-          console.log("6초 메시지 전송");
         }
         else if(ms == 3999) {
           io.to(room[Index].roomCode).emit('ox_during', {during_time : 6, _answer : random_quiz(ms, Index)});
           resolve(ms);
-          console.log("4초 메시지 전송");
         }
         else if(ms == 999)  {
           io.to(room[Index].roomCode).emit('ox_breaking', {break_time : 4, _question : random_quiz(ms, Index)});
           resolve(ms);
-          console.log("1초 메시지 전송");
-        }else { reject(new Error("fail"))}
+        }
       },ms);
     })
   };
 
-  const cycle1 = async (Index)=>{
-    const isbreak = await oxcycle1(BREAK_DUR_TIME, Index)
-    .then((data) => {console.log(`4초 결과 : ${data}`);} , 
-    (Error) => {console.log(`4초 에러 : ${Error}`)})
-    const isduring = await oxcycle1(QUIZ_DUR_TIME, Index)
-    .then((data) => {console.log(`6초 결과 : ${data}`);} ,
-    (Error) => {console.log(`6초 에러 : ${Error}`)})
+  const cycle = async (Index) => {
+    console.log(`퀴즈 체크 배열1 : ${room[Index].cur_quiz_index.length}`);
+    const isbreak = await oxcycle(BREAK_DUR_TIME, Index)
+    console.log(`퀴즈 체크 배열2 : ${room[Index].cur_quiz_index.length}`);
+    const isduring = await oxcycle(QUIZ_DUR_TIME, Index)
+    console.log(`퀴즈 체크 배열3 : ${room[Index].cur_quiz_index.length}`);
     if(room[Index].cur_quiz_index.length <= 8) {
-      const ischeck = await oxcycle1(CHECK_DUR_TIME, Index)
-      .then((data) => {console.log(`1초 결과 : ${data}`);} ,
-      (Error) => {console.log(`1초 에러 : ${Error}`)})
-    }else o.to(room[Index].roomCode).emit('ox_end')
+      const ischeck = await oxcycle(CHECK_DUR_TIME, Index)
+    }else io.to(room[Index].roomCode).emit('ox_end')
   };
+
+  const quiz_cycle = async (Index) => {
+    console.log('퀴즈 사이클 진입');
+    for await (var value of quiz_num) {
+      console.log('for await of 진입');
+      await cycle(Index);
+      console.log(`퀴즈 사이클 실행 횟수 : ${ value }`);
+    }
+  }
 
   socket.on('쥰비완료쓰', (id)=>{
     let Index = room.findIndex(e => e.userid.includes(id));
@@ -75,7 +72,7 @@ module.exports = (io, socket, room) => {
       let player = room[Index].players;
       if(room[Index].cnt == player.length) {
           io.to(room[Index].roomCode).emit('ox_breaking', {break_time : 4, _question : random_quiz(999, Index)});
-          cycle1(Index)
+        quiz_cycle(Index);
       }
     }
   })
