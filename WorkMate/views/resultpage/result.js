8ba324c3a767d9f81b11418fcb0251adc38fd319
leegeunwoo <67493361+riulwoo@users.noreply.github.com>
2022-10-26 06:52:40 +0000

    ...

--- WorkMate/views/resultpage/result.js
+++ WorkMate/views/resultpage/result.js
@@ -1,10 +1,12 @@
 // sortedScore = 유저의 id, score, nick
 // name / score 클래스의 innerText에 넣기
+let ranks = document.querySelectorAll('.rank');
 let names = document.querySelectorAll('.name');
 let scores = document.querySelectorAll('.score');
 
 for(let i = 0; i < sortedScore.length; i++)
 {
+  ranks[i].innerText = `${i+1}등`;
   names[i].innerText = sortedScore[i].nick;
   scores[i].innerText = sortedScore[i].score;
 }
