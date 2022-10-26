00db39a13c62b4536ee27a753d3ada0c0222a764
mins <80028813+minseong00@users.noreply.github.com>
2022-10-26 06:36:25 +0000

    20221026 resultPage data insert

--- WorkMate/views/resultpage/result.js
+++ WorkMate/views/resultpage/result.js
@@ -1,5 +1,14 @@
-console.log(sortedScore[0]);
-console.log(sortedScore[1]);
-console.log(sortedScore[0].id);
-console.log(sortedScore[0].score);
-console.log(sortedScore[0].nick);
+// sortedScore = 유저의 id, score, nick
+// name / score 클래스의 innerText에 넣기
+let names = document.querySelectorAll('.name');
+let scores = document.querySelectorAll('.score');
+
+for(let i = 0; i < sortedScore.length; i++)
+{
+  names[i].innerText = sortedScore[i].nick;
+  scores[i].innerText = sortedScore[i].score;
+}
+
+document.getElementById('first').innerText = sortedScore[0].nick;
+document.getElementById('second').innerText = sortedScore[1].nick;
+document.getElementById('third').innerText = sortedScore[2].nick;
