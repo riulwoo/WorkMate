7aa1c1c5a27c7cd1ccb19be638f7da414a8237a2
냐옹이 <wjddms2458@icloud.com>
2022-11-02 02:29:51 +0000

    ...

---
+++ WorkMate/view/result_page_backup/index.js
@@ -0,0 +1,16 @@
+// sortedScore = 유저의 id, score, nick
+// name / score 클래스의 innerText에 넣기
+let ranks = document.querySelectorAll('.rank');
+let names = document.querySelectorAll('.name');
+let scores = document.querySelectorAll('.score');
+
+for(let i = 0; i < sortedScore.length; i++)
+{
+  ranks[i].innerText = `${i+1}등`;
+  names[i].innerText = sortedScore[i].nick;
+  scores[i].innerText = sortedScore[i].score;
+}
+
+document.getElementById('first').innerText = sortedScore[0].nick;
+document.getElementById('second').innerText = sortedScore[1].nick;
+document.getElementById('third').innerText = sortedScore[2].nick;
