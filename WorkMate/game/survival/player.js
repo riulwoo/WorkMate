1352c8922ee9bb78fd127d32d4b189cebcb34b98
mins <80028813+minseong00@users.noreply.github.com>
2022-11-04 06:25:56 +0000

    ...

--- WorkMate/game/survival/player.js
+++ WorkMate/game/survival/player.js
@@ -18,7 +18,7 @@ function surviv_renderPlayer() {
 
     surv_ctx.fillStyle = player.color;
     surv_ctx.font = "bold 20px DungGeunMo";
-    surv_ctx.fillText(player.nick, player.x + 25, player.y - player.radius + 10);
+    surv_ctx.fillText(player.nick, player.x, player.y - player.radius);
 
     surv_ctx.closePath();
 
